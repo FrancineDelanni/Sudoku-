28ddc7519a275d6572c8aeb1bdb15bbe8955c982
Francine Delanni <delannifrancine@gmail.com>
2025-07-17 06:41:40 -0300

    create components table, cell and testing components

---
+++ src/components/table/table.tsx
@@ -0,0 +1,13 @@
+import './table.css'
+
+function Table(){
+    return(
+        <div className="wrapper">
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Table;
---
+++ src/pages/game.tsx
@@ -0,0 +1,11 @@
+import Table from '../components/table/table'
+
+function GamePage(){
+    return(
+        <div className="wrapper">
+            <Table />
+        </div>
+    )
+}
+
+export default GamePage;
